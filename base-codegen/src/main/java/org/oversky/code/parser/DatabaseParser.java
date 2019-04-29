b6c6c2a787ded4b8d64105e79137f820ab2f225b
mopeleo <mopeleo@126.com>
2019-04-29 16:16:50 +0800

    'update'

--- base-codegen/src/main/java/org/oversky/code/parser/DatabaseParser.java
+++ base-codegen/src/main/java/org/oversky/code/parser/DatabaseParser.java
@@ -19,7 +19,11 @@ public class DatabaseParser {
 	private static String QUERY_ALL_TABLES_MYSQL = "select table_name,table_comment from information_schema.tables " 
 			+ " where table_schema = (select database())";
 	
-	private static String QUERY_TABLE_ORACLE = "";
+	private static String QUERY_TABLE_ORACLE = "select b.comments,a.column_name,a.data_type,a.data_length,a.data_precision,a.data_scale,a.column_id,a.nullable,a.data_default,e.constraint_type "
+			+ " from user_tab_columns a join user_col_comments b on a.table_name=b.table_name and a.column_name=b.column_name "
+			+ " left join (select d.table_name,d.column_name,c.constraint_type "
+			+ "     from user_constraints c,user_cons_columns d where c.constraint_name=d.constraint_name and c.constraint_type='P') e "
+			+ " on b.table_name=e.table_name and b.column_name=e.column_name where lower(a.table_name) = ?";
 	private static String QUERY_TABLE_MYSQL = "select column_name,data_type,column_type,column_comment,column_key,column_default,"
 			+ " character_maximum_length,numeric_precision,numeric_scale,is_nullable,extra from information_schema.columns t "
 			+ " where table_schema = (select database()) and table_name = ?";
@@ -42,7 +46,7 @@ public class DatabaseParser {
 				table.setDbms(dbms);
 				table.setId(tableName);
 				table.setOriginCode(tableName);
-				if(tableComment.toLowerCase().indexOf("[cache]") >= 0) {
+				if(tableComment != null && tableComment.toLowerCase().indexOf("[cache]") >= 0) {
 					table.setCache(true);
 				}
 				String code = "";
@@ -134,6 +138,57 @@ public class DatabaseParser {
 	private static void fillOracleTable(Table tab, ResultSet rs) throws SQLException {
 		while(rs.next()) {
 			Column col = new Column();
+			col.setId(rs.getString("column_id"));
+			col.setComment(rs.getString("comments") == null ? "" : rs.getString("comments"));
+			col.setDefaultValue(rs.getString("data_default"));
+			col.setLength(rs.getString("data_length"));
+			String realLen = rs.getString("data_precision");
+			if(realLen != null && !"".equals(realLen)) {
+				col.setLength(realLen);
+			}
+			col.setPrecision(rs.getString("data_scale"));
+			String nullAble = rs.getString("nullable").toUpperCase();
+			if("N".equals(nullAble)) {
+				col.setMandatory("1");
+			}else {
+				col.setMandatory("0");
+			}
+			//把数字型转换为NUMBER(P,S)的类型
+			String type = rs.getString("data_type").toUpperCase();
+			if("NUMBER".equals(type)) {
+				if(col.getPrecision() != null && Integer.parseInt(col.getPrecision()) > 0) {
+					type = type + "("+ col.getLength() +"," + col.getPrecision() + ")";
+				}else {
+					type = type + "("+ col.getLength() + ")";
+				}
+			}
+			col.setOriginDatatype(type);
+			col.setDatatype(DataType.convertType(col.getOriginDatatype(), DataType.SourceType.PDM_ORACLE));
+			String columnName = rs.getString("column_name").toLowerCase();
+			col.setOriginCode(columnName);
+            String code = "";
+            if(columnName.indexOf(DataType.CODE_SEP) > 0){
+                String[] parts = columnName.split(DataType.CODE_SEP);
+                code = parts[0];
+                for(int p = 1; p < parts.length; p++){
+                    code += parts[p].substring(0, 1).toUpperCase() + parts[p].substring(1);
+                }
+            }else{
+                code = columnName;
+            }
+            col.setCode(code);
+            col.setName(code);
+
+            // oracle无法设置递增字段
+			col.setIdentity("0");
+			tab.addColumn(col);
+
+            String pri = rs.getString("constraint_type");
+            if("P".equals(pri)) {
+            	tab.addKey(col);
+            }else {
+            	tab.addColsExceptKey(col);
+            }
 		}
 	}
 	
@@ -148,6 +203,17 @@ public class DatabaseParser {
 		}
 	}
 	
+	private static String getTableColumnList(DataType.SourceType type) {
+		switch(type) {
+			case PDM_ORACLE :
+				return QUERY_TABLE_ORACLE;
+			case PDM_MYSQL:
+				return QUERY_TABLE_MYSQL;
+			default:
+				throw new NullPointerException("不支持的数据类型");
+		}
+	}
+	
 	private static void fillTable(DataType.SourceType type, Table tab, ResultSet rs) throws SQLException {
 		switch(type) {
 			case PDM_ORACLE :
@@ -156,17 +222,6 @@ public class DatabaseParser {
 			case PDM_MYSQL:
 				fillMysqlTable(tab, rs);
 				break;
-			default:
-				throw new NullPointerException("不支持的数据类型");
-		}
-	}
-	
-	private static String getTableColumnList(DataType.SourceType type) {
-		switch(type) {
-			case PDM_ORACLE :
-				return QUERY_TABLE_ORACLE;
-			case PDM_MYSQL:
-				return QUERY_TABLE_MYSQL;
 			default:
 				throw new NullPointerException("不支持的数据类型");
 		}
